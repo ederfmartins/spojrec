9885ebb4fd8f54e0ff74425fc6066975874e61a9
Eder <ederfmartins@gmail.com>
2014-11-23 09:54:14 -0200

    adding config to solving scrapy bind problem. 	new file:   spojrec/src/crawler/crawlerrunner.py

---
+++ spojrec/src/crawler/crawlerrunner.py
@@ -0,0 +1,20 @@
+from spojrec.src.basicdefs import DATA_DIR, SCRAPY_BIND_ADDRESS
+
+if __name__ == '__main__':
+   
+  config_values = {
+    'bind_address': SCRAPY_BIND_ADDRESS,
+    'http_port': SCRAPY_HTTP_PORT,
+    'logs_dir': LOGS_DIR,
+    'eggs_dir': DATA_DIR + 'eggs',
+    'dbs_dir': DATA_DIR + 'dbs',
+    'items_dir': DATA_DIR + 'items'
+  }
+
+  scrapyd_conf = "[scrapyd]\n" + \
+    "\n".join('{}={}'.format(key, val) for key, val in config_values.items())
+ 
+  with open('scrapyd.conf', 'w') as f:
+    f.write(scrapyd_conf)
+
+  os.system("scrapyd") 
